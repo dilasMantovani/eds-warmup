b4a038ef753d1f8aaa8cb52ff0695c67383f1928
Diego Dias <diego.dias@vilt.local>
2024-09-16 11:30:06 -0300

    fixed tab behaviour when multiple tabs are inserted

--- blocks/controlador-das-abas/controlador-das-abas.js
+++ blocks/controlador-das-abas/controlador-das-abas.js
@@ -1,7 +1,18 @@
-const allTabSections = document.querySelectorAll('[data-tab-id]');
-const tabData = Array.from(allTabSections).map((tabElement) => {return {'tabId' : tabElement.getAttribute('data-tab-id'), 'tabTitle': tabElement.getAttribute('data-tab-title')}})
-
 export default function decorate(block) {
+
+    let tabsToRender = null;
+    if(block?.children[0]?.textContent?.trim() !== ""){
+        tabsToRender = block?.children[0]?.textContent?.trim()?.split(',');
+    }
+    let allTabSections;
+    if(tabsToRender){
+        const selector = tabsToRender?.map(id => `[data-tab-id="${id}"]`)?.join(',');
+        allTabSections = document.querySelectorAll(selector);
+    }else{
+        allTabSections = document.querySelectorAll('[data-tab-id]');
+    }
+
+    const tabData = Array.from(allTabSections)?.map((tabElement) => {return {'tabId' : tabElement.getAttribute('data-tab-id'), 'tabTitle': tabElement.getAttribute('data-tab-title')}})
     let tabListItems = "";
     tabData.forEach(tab=>{
         tabListItems += `<li><a>${tab?.tabTitle}</a></li>`;
@@ -9,12 +20,12 @@ export default function decorate(block) {
 
     const outputHtml = `
         <div class="controlador-das-abas-wrapper">
-            <ul>
+            <ul tab-group="${tabData?.map(tab=> {return tab?.tabId})?.join(",")}">
                 ${tabData?.map(tab=>{
                     return `<li>
                                 <a role="tab" tab-points-to="${tab?.tabId}">${tab?.tabTitle}</a>
                             </li>`;
-                }).join("")
+                })?.join("")
                 }
             </ul>
         </div>
@@ -28,10 +39,11 @@ export default function decorate(block) {
         tab.addEventListener('click', handleTabChange);
     });
 
-    init();
+    init(tabData);
 }
 
-function init(){
+function init(tabData){
+    
     if(!tabData) return;
 
     const tabId = tabData[0].tabId;//first tab
@@ -44,10 +56,17 @@ function init(){
 }
 
 function handleTabChange(e){
+
+    const tabGroup = e.target.parentNode.parentNode.getAttribute('tab-group').split(',');
+    const selector = tabGroup?.map(id => `[data-tab-id="${id}"]`)?.join(',');
+    const groupTabSections = document.querySelectorAll(selector);
+    const tabData = Array.from(groupTabSections)?.map((tabElement) => {return {'tabId' : tabElement.getAttribute('data-tab-id'), 'tabTitle': tabElement.getAttribute('data-tab-title')}})
+
     e.target.classList.add("active");
 
     const tabId = e.target.getAttribute('tab-points-to');
     document.querySelector(`[data-tab-id="${tabId}"]`).classList.remove("hidden");
+    
     tabData.filter(tab => tab?.tabId !== tabId).forEach(tab => {
         document.querySelector(`[data-tab-id="${tab?.tabId}"]`).classList.add("hidden");
         document.querySelector(`[tab-points-to="${tab?.tabId}"]`).classList.remove("active");
