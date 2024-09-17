1218dcd3a3fbe5f7cb29f6c9dc0a45504d4c7c0c
Diego Dias <diego.dias@vilt.local>
2024-09-17 17:47:43 -0300

    added script loading to delayed.js

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,12 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+window.Prism = window.Prism || {};
+window.Prism.manual = true;
+import('./prism.js')
+  .then(() => {
+    // run prism in async mode; uses webworker.
+    window.Prism.highlightAll();
+  })
+  // eslint-disable-next-line no-console
+  .catch((err) => console.error(err));
