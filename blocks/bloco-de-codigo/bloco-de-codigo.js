6bf76400ea7153f1b4f7ad9c718cdba82ee0cf9f
Diego Dias <diego.dias@vilt.local>
2024-09-18 09:26:13 -0300

    code-block styling final adjustments

--- blocks/bloco-de-codigo/bloco-de-codigo.js
+++ blocks/bloco-de-codigo/bloco-de-codigo.js
@@ -1,20 +1,9 @@
 export default function decorate(block) {
     const language = block?.children[1]?.textContent?.trim();
 
-    console.log(language)
-
     block?.children[1]?.remove()
 
     block.classList.add("line-numbers");
     block.querySelector('code').classList.add(`language-${language}`)
 
-    window.Prism = window.Prism || {};
-    window.Prism.manual = true;
-    import('../../scripts/prism.js')
-      .then(() => {
-        // run prism in async mode; uses webworker.
-        window.Prism.highlightAll();
-      })
-      // eslint-disable-next-line no-console
-      .catch((err) => console.error(err));
 }
--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,22 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+function highlightCodeBlock(){
+    const highlightable = document.querySelector(
+        'code[class*="language-"], [class*="language-"] code, code[class*="lang-"], [class*="lang-"] code',
+      );
+    if (!highlightable) return; // exit, no need to load prism if nothing to highlight
+
+    window.Prism = window.Prism || {};
+    window.Prism.manual = true;
+    import('./prism.js')
+      .then(() => {
+        // run prism in async mode; uses webworker.
+        window.Prism.highlightAll();
+      })
+      // eslint-disable-next-line no-console
+      .catch((err) => console.error(err));
+}
+
+highlightCodeBlock()
