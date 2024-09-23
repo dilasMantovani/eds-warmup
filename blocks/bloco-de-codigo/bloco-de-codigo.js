67454ddeb636585d9bc5a7d0493315cf0290cb8e
Diego Dias <diego.dias@vilt.local>
2024-09-23 10:30:45 -0300

    bugfix on code-block

--- blocks/bloco-de-codigo/bloco-de-codigo.js
+++ blocks/bloco-de-codigo/bloco-de-codigo.js
@@ -3,7 +3,7 @@ export default function decorate(block) {
 
     block?.children[1]?.remove()
 
-    block.classList.add("line-numbers");
-    block.querySelector('code').classList.add(`language-${language}`)
+    block?.classList?.add("line-numbers");
+    block?.querySelector('code')?.classList?.add(`language-${language}`)
 
 }
