396633b1f13613b323f832fffa1216b39876eded
Diego Dias <diego.dias@vilt.local>
2024-09-30 10:39:15 -0300

    added layout for button modal

---
+++ blocks/botao-expansivel/botao-expansivel.js
@@ -0,0 +1,68 @@
+import { htmlToElement } from "../../scripts/scripts.js";
+
+export default function decorate(block) {
+    const cta = block.children[0];
+    const title = block.children[1];
+    const text = block.children[2];
+    const image = block.children[3];
+    const imgTitle = block.children[4];
+    const description = block.children[5];
+
+    const ctaText = cta?.textContent?.trim();
+    const titleText = title?.textContent?.trim();
+    const imgTitleText = imgTitle?.textContent?.trim();
+    const descriptionText = description?.textContent?.trim();
+
+    const button = htmlToElement(`<a class="btn-modal" href="#">${ctaText}</a>`);
+    block.textContent="";
+    block.append(button);
+
+    const modalElement = htmlToElement(`
+        <div class="modal">
+            <div class="modal-content">
+                <div class="modal-content-header">
+                    <h4 class="modal-content-header-title">${titleText}</h4>
+                    <i class="fa-solid fa-xmark"></i>
+                </div>
+                <div class="modal-content-body">
+                    ${text?.innerHTML}
+                    ${image?.querySelector("img") ?
+                        `
+                        <div class="modal-content-body-img-wrapper">
+                            <p>${imgTitleText}</p>
+                            ${image.innerHTML}
+                            <p>${descriptionText}</p>
+                        </div>
+                        `
+                     : ""}
+                </div>
+                <div class="modal-content-footer">
+                
+                </div>
+            </div>
+        </div>
+    `)
+
+    block.append(modalElement);
+
+    const contentSectionElement = modalElement.querySelector(".modal-content");
+    button.addEventListener('click', () => {
+
+        modalElement.style.display = "block";
+        setTimeout(() => {
+            modalElement.style.opacity = 1;
+            contentSectionElement.classList.add("active")
+        }, 100);
+    });
+
+    modalElement.addEventListener('click', (e) => {
+        if(e.target === modalElement || e.target?.className.includes("fa-xmark")){
+            modalElement.style.opacity = 0;
+            contentSectionElement.classList.remove("active")
+            setTimeout(() => {
+                modalElement.style.display = "none";
+
+            }, 500);
+        }
+    });
+}
