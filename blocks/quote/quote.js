4d8ae050dc625b60bf8d86936b9fbbb9ffaae72b
Vinicius Moraes <vinicius.moraes@vilt-group.com>
2024-10-08 10:33:19 -0300

    :sparkles: create variant of quote design - update quote design

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -3,14 +3,13 @@ export default function decorate(block) {
     const quoteImage = block.children[1].querySelector("img");
     const quoteText = block.children[2]?.textContent?.trim();
     const quoteAuthor = block.children[3]?.textContent?.trim();
-
-    console.log(quoteImage);
+    const quoteIcon = block.children[4]?.textContent?.trim();
 
     const quote = () => {
-        if(quoteType === "quote") return(
-            `
+        if(quoteType === "quote") {
+            return(`
                 <section class="quote-author">
-                    <img src="${quoteImage}" />
+                    <img src="${quoteImage || '/blocks/quote/quote.png'}" />
                     <blockquote>
                         <p>${quoteText}</p>
                         <footer>
@@ -18,17 +17,20 @@ export default function decorate(block) {
                         </footer>
                     </blockquote>
                 </section>
-            `)
-        else return(
-            `
+            `);
+        } else {
+            console.log("BATATA")
+            return(`
                 <section class="quote-phrase">
-                    <img src="${quoteImage.src}" />
+                    <i class="fa fa-${quoteIcon || 'coffee'}"></i>
                     <blockquote>
                         <p>${quoteText}</p>
                     </blockquote>
                 </section>
             `)
-    }
 
+        }
+    }
+    
     block.innerHTML = quote();
 }
