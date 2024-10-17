be51b82f5a5c8c5f1084b2469b533bf5b7894163
Vinicius Moraes <vinicius.moraes@vilt-group.com>
2024-10-17 18:01:44 -0300

    :clown_face: debugging component

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -10,23 +10,28 @@ function highlightCodeBlock(){
     const highlightable = document.querySelector(
         'code[class*="language-"], [class*="language-"] code, code[class*="lang-"], [class*="lang-"] code',
       );
-    if (!highlightable) return; // exit, no need to load prism if nothing to highlight
 
-    window.Prism = window.Prism || {};
-    window.Prism.manual = true;
-    import('./prism.js')
+      console.log("Entrou");
+      if (!highlightable) return; // exit, no need to load prism if nothing to highlight
+      
+      console.log("Seguiu");
+      
+      window.Prism = window.Prism || {};
+      window.Prism.manual = true;
+      import('./prism.js')
       .then(() => {
         // run prism in async mode; uses webworker.
-        setTimeout(() => {
-          window.Prism.highlightAll();
-          console.log("Carregou?!")
-        }, 2000);
+        console.log("Marcou");
+        window.Prism.highlightAll();
       })
       // eslint-disable-next-line no-console
       .catch((err) => console.error(err));
 }
 
-highlightCodeBlock();
+  setTimeout(() => {
+    console.log("Entrando")
+    highlightCodeBlock();
+  }, 2000);
 
 baSlider(".comparison-slider");
 
