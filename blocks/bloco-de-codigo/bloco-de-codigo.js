946397b77856751b7e3de454bca2225de9a2c03a
Vinicius Moraes <vinicius.moraes@vilt-group.com>
2024-10-17 17:39:24 -0300

    :clown_face: debugging component

--- blocks/bloco-de-codigo/bloco-de-codigo.js
+++ blocks/bloco-de-codigo/bloco-de-codigo.js
@@ -3,7 +3,9 @@ import { isInEditor } from "../../scripts/scripts.js";
 export default function decorate(block) {
     const language = block?.children[1]?.textContent?.trim();
 
-    block?.children[1]?.remove()
+    // block?.children[1]?.remove()
+
+    window.block = block;
 
     block?.classList?.add("line-numbers");
     block?.querySelector('code')?.classList?.add(`language-${language}`);
