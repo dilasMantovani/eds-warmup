7b0a4c3d4d46d683ff2755c75e1d1b4fb04056bd
Diego Dias <diego.dias@vilt.local>
2024-11-28 13:40:39 -0300

    reverting useless instrumentation code

--- blocks/texto-rico/texto-rico.js
+++ blocks/texto-rico/texto-rico.js
@@ -1,6 +1,2 @@
-import { moveInstrumentation } from "../../scripts/scripts.js"
-
 export default function decorate(block) {
-  const fakeElement = document.createElement("div");
-  moveInstrumentation(block.children[0], fakeElement)
 }
