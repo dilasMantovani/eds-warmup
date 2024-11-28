9b624d717588f883ba1fecc989f5bf7e92929dc1
Diego Dias <diego.dias@vilt.local>
2024-11-28 16:13:50 -0300

    adjusted aue remover

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -251,7 +251,8 @@ export function removeDataAueAttributesWhenThereIsFormula(element) {
 
   //Se não houver fórmula, morre aqui
   const body = element.textContent;
-  if(body?.includes("##") || body?.includes("$$")) {
+  if(body?.includes("##") || body?.includes("$$") || body.querySelectorAll("mjx-container").length > 0) {
+    console.log(body)
     // Seleciona todos os elementos filhos do elemento fornecido
     const children = element.querySelectorAll("*");
   
