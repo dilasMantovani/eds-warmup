719fa44957b00cb976aa08f5e66c7e2ef3af29bd
Diego Dias <diego.dias@vilt.local>
2024-11-28 13:57:23 -0300

    attempt to remove inline editing

--- blocks/texto-rico/texto-rico.js
+++ blocks/texto-rico/texto-rico.js
@@ -1,2 +1,23 @@
 export default function decorate(block) {
+  removeDataAueAttributes(block)
+}
+
+function removeDataAueAttributes(element) {
+  if (!element || !(element instanceof HTMLElement)) {
+      console.error("O argumento fornecido não é um elemento HTML válido.");
+      return;
+  }
+
+  // Seleciona todos os elementos filhos do elemento fornecido
+  const children = element.querySelectorAll("*");
+
+  children.forEach(child => {
+      // Itera sobre os atributos do elemento filho
+      Array.from(child.attributes).forEach(attr => {
+          // Verifica se o nome do atributo começa com "data-aue"
+          if (attr.name.startsWith("data-aue")) {
+              child.removeAttribute(attr.name); // Remove o atributo
+          }
+      });
+  });
 }
