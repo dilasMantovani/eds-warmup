d01a0cc50108ac43929d1bc24dff5c5db85b95c1
Diego Dias <diego.dias@vilt.local>
2024-12-05 09:53:53 -0300

    added layout and functionality to galery component

--- blocks/galeria/galeria.js
+++ blocks/galeria/galeria.js
@@ -1,4 +1,188 @@
+import { isInEditor } from "../../scripts/scripts.js";
+
 export default function decorate(block) {
-  console.log(block)
+  const variant = block?.children[0];
+  const variantText = variant?.textContent?.trim();
+  variant.remove()
+
+  const texts = [];
+  const images = [];
+
+  block.classList.add(variantText)
+
+  Array.from(block?.children).forEach(element => {
+    const type = element?.children[0];
+    const typeText = type?.textContent?.trim();
+    type.remove();
+
+    if (typeText === "txt") texts.push(element)
+    if (typeText === "img") images.push(element)
+
+    element.remove();
+  });
+
+  //validando se a relação de imagens realmente está 1/1
+  if(variantText === 'text-and-image-1-1' && texts.length !== images.length){
+    if(!isInEditor) return;
+    const imageAndTextAmountDontMatchErrorHTML = `
+            <div class="oaerror error">
+                <strong>Atenção</strong> - Nesta variação da galeria a quantidade de imagens e textos deve ser o mesmo.
+            </div>
+        `;
+    
+        var msgElement = document.createElement('div');
+        msgElement.classList.add("error-notice");
+        msgElement.innerHTML += imageAndTextAmountDontMatchErrorHTML;
+        block.insertBefore(msgElement, block.firstChild);
+    return;
+  }
+
+  const arrowsHTML =`
+          <div class="splide__arrows">
+            <button class="splide__arrow splide__arrow--prev">
+              <i class="fa-solid fa-circle-chevron-left"></i>
+            </button>
+            <button class="splide__arrow splide__arrow--next">
+              <i class="fa-solid fa-circle-chevron-right"></i>
+            </button>
+          </div>
+  `
+
+  switch (variantText) {
+    case 'image-only':
+      block.innerHTML += `
+        <div class="splide" role="group">
+          ${arrowsHTML}
+          <div class="splide__track">
+            <ul class="splide__list">
+              ${images?.map(img => {
+                return `<li class="splide__slide">
+                          <div class="splide__slide__container">
+                            ${handleImage(img)}
+                          </div>
+                        </li>`;
+                })?.join("")
+              }
+            </ul>
+          </div>
+        </div>
+    `
+    break;
+    case 'text-only':
+      block.innerHTML += `
+        <div class="splide" role="group">
+          ${arrowsHTML}
+          <div class="splide__track">
+            <ul class="splide__list">
+              ${texts?.map(text => {
+                return `<li class="splide__slide">
+                          <div class="splide__slide__container">
+                            ${text.outerHTML}
+                          </div>
+                        </li>`;
+                })?.join("")
+              }
+            </ul>
+          </div>
+        </div>
+    `
+    break;
+    case 'text-and-image-1-1':
+      block.innerHTML += `
+        <div class="splide" role="group">
+          ${arrowsHTML}
+          <div class="splide__track">
+            <ul class="splide__list">
+              ${texts?.map((text, index) => {
+                return `<li class="splide__slide">
+                          <div class="splide__slide__container text-and-image-1-1">
+                            ${text.outerHTML}
+                            ${handleImage(images[index])}
+                          </div>
+                        </li>`;
+                })?.join("")
+              }
+            </ul>
+          </div>
+        </div>
+    `
+    break;
+    case 'one-image-many-texts':
+      block.innerHTML += `
+        <div class="splide" role="group">
+          ${arrowsHTML}
+          <div class="splide__track">
+            <ul class="splide__list">
+              ${texts?.map((text) => {
+                return `<li class="splide__slide">
+                          <div class="splide__slide__container">
+                            ${text.outerHTML}
+                          </div>
+                        </li>`;
+                })?.join("")
+              }
+            </ul>
+          </div>
+        </div>
+        <div class="image__container">
+            ${handleImage(images[0])}
+        </div>
+    `
+    break;
+    case 'one-text-many-images':
+      block.innerHTML += `
+        <div class="text__container">
+            ${texts[0]?.outerHTML}
+        </div>
+        <div class="splide" role="group">
+          ${arrowsHTML}
+          <div class="splide__track">
+            <ul class="splide__list">
+              ${images?.map((img) => {
+                return `<li class="splide__slide">
+                          <div class="splide__slide__container">
+                            ${handleImage(img)}
+                          </div>
+                        </li>`;
+                })?.join("")
+              }
+            </ul>
+          </div>
+        </div>
+    `
+    break;
+    default:
+      break;
+  }
+
+  var elms = block.getElementsByClassName('splide');
+
+  for (var i = 0; i < elms.length; i++) {
+    new Splide(elms[i], {
+      rewind: true,
+      rewindSpeed: 1000,
+      pagination: true
+    }).mount();
+  }
 }
 
+function handleImage(imageElement){
+  const img = imageElement?.children[0];
+  const title = imageElement?.children[1].textContent.trim();
+  const desc = imageElement?.children[2].textContent.trim();
+
+  const pictureElement = img.querySelector("picture");
+  const imgHeader = document.createElement("figcaption");
+  const imgFooter = document.createElement("figcaption");
+  
+  imgHeader.textContent = title;
+  imgHeader.classList.add("img-header")
+  pictureElement.insertBefore(imgHeader, pictureElement.firstChild)
+  
+  imgFooter.textContent = desc;
+  imgFooter.classList.add("img-footer")
+  pictureElement.appendChild(imgFooter)
+  
+
+  return pictureElement?.outerHTML;
+}
