4dbbd3f586fce74ac9db8a8934bda3cc4b5696d2
Diego Dias <diego.dias@vilt.local>
2024-12-09 11:17:07 -0300

    renamed component

--- blocks/mapa-mental-organograma-fluxograma/mapa-mental-organograma-fluxograma.js
+++ blocks/mapa-mental-organograma-fluxograma/mapa-mental-organograma-fluxograma.js
@@ -1,6 +1,7 @@
 export default function decorate(block) {
   const content = block.children[0]?.textContent?.trim();
 
+  console.log(content)
   block.innerHTML = `<pre class="mermaid">${content}</pre>`;
 }
 
