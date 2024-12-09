c6ff39c54239bd4581e6a65ead37cf6c9f822549
Diego Dias <diego.dias@vilt.local>
2024-12-09 11:40:06 -0300

    added log to check the rte output within universal editor

--- blocks/mapamentalorganogramafluxograma/mapamentalorganogramafluxograma.js
+++ blocks/mapamentalorganogramafluxograma/mapamentalorganogramafluxograma.js
@@ -3,6 +3,7 @@ export default function decorate(block) {
 
   console.log(block.children[0]?.textContent)
   console.log(block.children[0]?.innerText)
+  console.log(block.children[0]?.innerHTML)
 
   block.innerHTML = `<pre class="mermaid">${content}</pre>`; 
 }
