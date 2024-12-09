4951d2f815bb359a78c5f424cdedb0722406892d
Diego Dias <diego.dias@vilt.local>
2024-12-09 11:54:17 -0300

    added html resolving routine

--- blocks/mapamentalorganogramafluxograma/mapamentalorganogramafluxograma.js
+++ blocks/mapamentalorganogramafluxograma/mapamentalorganogramafluxograma.js
@@ -1,10 +1,21 @@
+import { isInEditor } from "../../scripts/scripts.js";
+
 export default function decorate(block) {
-  const content = block.children[0]?.textContent?.trim();
+  let content = block.children[0]?.textContent?.trim();
+  console.log(isInEditor())
+  if(isInEditor()){
+    content = block.children[0]?.querySelector(pre)?.innerHTML
+    if(content){
+      content = decodeHtml(content);
+    }
+  }
 
-  console.log(block.children[0]?.textContent)
-  console.log(block.children[0]?.innerText)
-  console.log(block.children[0]?.innerHTML)
-
+  console.log(content)
   block.innerHTML = `<pre class="mermaid">${content}</pre>`; 
 }
 
+function decodeHtml(html) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = html;
+    return txt.value;
+}
