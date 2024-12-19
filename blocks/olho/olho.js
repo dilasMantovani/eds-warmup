0ce0f48357ed1de0d7b79128816912e1f4c1d07b
Diego Dias <diego.dias@vilt.local>
2024-12-19 16:36:05 -0300

    finished olho component

---
+++ blocks/olho/olho.js
@@ -0,0 +1,41 @@
+export default function decorate(block) {
+  const variant = block?.children[0];
+  const title = block?.children[1];
+  const content = block?.children[2];
+  const icon = block?.children[3];
+
+  const variantText = variant?.textContent?.trim();
+  const titleText = title?.textContent?.trim();
+  const contentText = content?.textContent?.trim();
+  const iconText = icon?.textContent?.trim();
+
+  const olho = () => {
+    if(variantText === "with-title") {
+        return(`
+            <section class="olho-with-title">
+                <div>
+                  <h4><strong>${titleText}</strong></h4>
+                </div>
+                <div>
+                  <blockquote>
+                    <p>${contentText}</p>
+                  </blockquote>
+                </div>
+            </section>
+        `);
+    } else {
+        return(`
+            <section class="olho-no-title">
+                <i class="fa fa-${iconText}"></i>
+                <blockquote>
+                    <p>${contentText}</p>
+                </blockquote>
+            </section>
+        `)
+
+    }
+}
+
+  block.innerHTML = olho();
+
+}
