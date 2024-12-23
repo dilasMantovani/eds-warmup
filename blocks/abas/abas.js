f7e946f9a2e416472b5b067ce8df11307dcadfbe
Diego Dias <diego.dias@vilt.local>
2024-12-23 13:05:37 -0300

    added behaviour to tab component and some adjustments on accordion

--- blocks/abas/abas.js
+++ blocks/abas/abas.js
@@ -1,2 +1,44 @@
+import { htmlToElement } from "../../scripts/scripts.js";
+
 export default function decorate(block) {
+  const tabs = [];
+
+  Array.from(block?.children).forEach((element, index) => {
+    const title = element.children[0];
+    const titleText = title.textContent;
+    title.remove();
+    tabs.push({title: titleText, body: element})
+    
+    if(index !== 0)
+      element.classList.add("hidden")
+  });
+
+  const outputHtml = htmlToElement(`
+  <div class="abas-header">
+      <ul>
+          ${tabs?.map((tab, index)=>{
+              return `<li>
+                          <a role="tab" index="${index}" class="${index == 0 ? "active" : ''}">${tab?.title}</a>
+                      </li>`;
+          })?.join("")
+          }
+      </ul>
+  </div>
+`);
+
+outputHtml?.querySelectorAll("a")?.forEach(tabTitle=>{
+  tabTitle?.addEventListener('click', (e) => {
+
+    outputHtml?.querySelectorAll("a")?.forEach(otherTab=>{
+      otherTab?.classList?.remove("active")    
+    });
+    e?.target?.classList?.add("active");
+    tabs?.forEach(element => {
+      element?.body?.classList?.add("hidden");
+    });
+    tabs[e?.target?.getAttribute("index")]?.body?.classList?.remove("hidden")
+  });
+})
+
+block?.insertBefore(outputHtml, block?.children[0])
 }
