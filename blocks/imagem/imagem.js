6e1efc68896d6e3d09dec282cfac86b8f9d0a4ff
Diego Dias <diego.dias@vilt.local>
2025-01-02 11:33:19 -0300

    added unique styling behaviour to img component

--- blocks/imagem/imagem.js
+++ blocks/imagem/imagem.js
@@ -1,4 +1,4 @@
-import { htmlToElement } from "../../scripts/scripts.js";
+import { generateUUID, htmlToElement } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
     const image = block.children[0];
@@ -6,62 +6,67 @@ export default function decorate(block) {
     const description = block.children[2];
     const zoomIn = block.children[3];
     const openModal = block.children[4];
+    const uniquecss = block.children[5];
 
     const titleText = title?.textContent.trim();
     const descriptionText = description?.textContent.trim();
     const zoomInVal = zoomIn?.textContent.trim();
     const openModalVal = openModal?.textContent.trim();
+    const uniquecssText = uniquecss?.textContent?.trim();
+
 
     title.remove();
     description.remove();
     zoomIn.remove();
     openModal.remove();
+    uniquecss.remove();
+
 
     const pictureElement = image.querySelector('picture');
 
-    if(!pictureElement) return;
+    if (!pictureElement) return;
 
 
-    if(titleText){        
+    if (titleText) {
         const imageTitleElement = document.createElement('p');
         imageTitleElement.textContent = titleText;
         block.insertBefore(imageTitleElement, image);
     }
 
-    if(descriptionText){
+    if (descriptionText) {
         const imageDescriptionElement = document.createElement('p');
         imageDescriptionElement.textContent = descriptionText;
         block.append(imageDescriptionElement)
     }
 
     //handleZoomIn
-    if(zoomInVal === "true"){
+    if (zoomInVal === "true") {
 
         pictureElement?.classList?.add("zoom-in");
 
         const imgElement = image?.querySelector('img');
-    
+
         pictureElement.addEventListener('mousemove', (e) => {
-        const rect = pictureElement.getBoundingClientRect();
-        const x = e.clientX - rect.left; // Posição X do mouse dentro do contêiner
-        const y = e.clientY - rect.top;  // Posição Y do mouse dentro do contêiner
-    
-        const moveX = (x / pictureElement.offsetWidth) * 100;
-        const moveY = (y / pictureElement.offsetHeight) * 100;
-    
-        imgElement.style.transformOrigin = `${moveX}% ${moveY}%`; // Define a origem do zoom
+            const rect = pictureElement.getBoundingClientRect();
+            const x = e.clientX - rect.left; // Posição X do mouse dentro do contêiner
+            const y = e.clientY - rect.top;  // Posição Y do mouse dentro do contêiner
+
+            const moveX = (x / pictureElement.offsetWidth) * 100;
+            const moveY = (y / pictureElement.offsetHeight) * 100;
+
+            imgElement.style.transformOrigin = `${moveX}% ${moveY}%`; // Define a origem do zoom
         });
-    
+
         pictureElement.addEventListener('mouseenter', () => {
-        imgElement.style.transform = 'scale(1.5)'; // Aplica o zoom de 50% quando o mouse entra
+            imgElement.style.transform = 'scale(1.5)'; // Aplica o zoom de 50% quando o mouse entra
         });
-    
+
         pictureElement.addEventListener('mouseleave', () => {
-        imgElement.style.transform = 'scale(1)'; // Retorna ao tamanho original quando o mouse sai
+            imgElement.style.transform = 'scale(1)'; // Retorna ao tamanho original quando o mouse sai
         });
     }
 
-    if(openModalVal === "true"){
+    if (openModalVal === "true") {
         pictureElement?.classList?.add("open-modal");
 
         const zoomIconElement = document.createElement("i");
@@ -93,7 +98,7 @@ export default function decorate(block) {
 
         //click fora do modal
         modalElement.addEventListener('click', (e) => {
-            if(e.target === modalElement || e.target?.className.includes("fa-xmark")){
+            if (e.target === modalElement || e.target?.className.includes("fa-xmark")) {
                 modalElement.style.opacity = 0;
                 setTimeout(() => {
                     modalElement.style.display = "none";
@@ -101,5 +106,16 @@ export default function decorate(block) {
             }
         });
     }
-    
+
+    if (uniquecssText) {
+        const uuid = generateUUID();
+        block.setAttribute("id", uuid)
+        const styleTag = document.createElement("style");
+        styleTag.textContent = `
+        #${uuid}{
+            ${uniquecssText}
+        }`;
+        block.append(styleTag)
+    }
+
 }
