dc8f7a3b6d4d31aa99b12202e85a09e4f1d1020d
Diego Dias <diego.dias@vilt.local>
2025-01-13 16:53:22 -0300

    added temp fake component to run experiments

---
+++ blocks/playground/playground.js
@@ -0,0 +1,18 @@
+export default function decorate(block) {
+  async function getData() {
+    const url = "https://publish-p136102-e1378103.adobeaemcloud.com/graphql/execute.json/vilt-group/getTabelaByPath;path=/content/dam/vilt-group/content-fragments/teste-tabela";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+  
+      const json = await response.json();
+      block.innerHTML = json.data.tabelaByPath.item.tabela.html;
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  getData();
+}
