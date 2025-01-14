c83b07d966d5883e1280412804aecee6b69257fc
Diego Dias <diego.dias@vilt.local>
2025-01-14 10:35:13 -0300

    added temp fake component to run experiments

--- blocks/playground/playground.js
+++ blocks/playground/playground.js
@@ -2,7 +2,7 @@ import { randomString } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
   async function getData() {
-    const url = `https://publish-p136102-e1378103.adobeaemcloud.com/graphql/execute.json/vilt-group/getTabelaByPath;path=/content/dam/vilt-group/content-fragments/teste-tabela?1=${randomString(6)}`;
+    const url = `https://publish-p136102-e1378103.adobeaemcloud.com/graphql/execute.json/vilt-group/getTabelaByPath;path=/content/dam/vilt-group/content-fragments/teste-tabela1`;
     try {
       const response = await fetch(url);
       if (!response.ok) {
