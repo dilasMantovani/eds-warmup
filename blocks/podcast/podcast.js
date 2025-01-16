29634fec664e2f200d1c3e6608693edb1d5293c8
Diego Dias <diego.dias@vilt.local>
2025-01-16 17:00:58 -0300

    added behaviour and theme

--- blocks/podcast/podcast.js
+++ blocks/podcast/podcast.js
@@ -1,10 +1,12 @@
-import { DAM_URL } from "../../scripts/scripts.js";
+import { DAM_URL, htmlToElement } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
   const path = block.children[0];
   const pathText = path.textContent.trim();
   path.remove();
 
-  block.innerHTML = `<audio controls src="${DAM_URL}${pathText}"></audio>`
+  const audioElement = htmlToElement(`<audio controls src="${DAM_URL}${pathText}"></audio>`);
+
+  block.append(audioElement);
   
 }
