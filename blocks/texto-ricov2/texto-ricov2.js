e0d4dc50cab3a88e81c50dd87e263aa19947ba5e
Diego Dias <diego.dias@vilt.local>
2025-02-06 14:55:00 -0300

    jodit experiment #2

--- blocks/texto-ricov2/texto-ricov2.js
+++ blocks/texto-ricov2/texto-ricov2.js
@@ -1,3 +1,4 @@
+import { htmlToElement } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
   const content = block.children[0];
@@ -14,9 +15,10 @@ export default function decorate(block) {
       const jodit = Jodit.make(editor, {
         "toolbarAdaptive": false      });
       jodit.e.on('blur', param => {
-        content.innerHTML = `${jodit.value.replaceAll("border-collapse:", "border-collapse: ")}`;
+        content.textContent = `${jodit.value.replaceAll("border-collapse:", "border-collapse: ")}`;
       });
   }, 1000);
 
+  block.append(htmlToElement(content.innerHTML))
 
 }
