7378deb3e8ed6ad219e204dd8c613aebc440cc1d
Diego Dias <diego.dias@vilt.local>
2025-02-06 15:22:58 -0300

    jodit experiment #4

--- blocks/texto-ricov2/texto-ricov2.js
+++ blocks/texto-ricov2/texto-ricov2.js
@@ -2,10 +2,11 @@ import { htmlToElement } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
   const content = block.children[0];
+  console.log(content)
 
 
   let editor = document.createElement('textarea');
-  editor.innerHTML = `${content.innerHTML}`
+  editor.innerHTML = `${atob(content.textContent)}`
 
 
   setTimeout(() => {
@@ -14,7 +15,7 @@ export default function decorate(block) {
       const jodit = Jodit.make(editor, {
         "toolbarAdaptive": false      });
       jodit.e.on('blur', param => {
-        content.children[0].textContent = `${btoa(jodit.value.replaceAll("border-collapse:", "border-collapse: "))}`;
+        content.querySelector("p").textContent = `${btoa(jodit.value.replaceAll("border-collapse:", "border-collapse: "))}`;
       });
   }, 1000);
 
