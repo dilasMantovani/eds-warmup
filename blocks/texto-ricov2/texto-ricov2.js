3d5464890570bbe542b43cf34291b5b2fec9301d
Diego Dias <diego.dias@vilt.local>
2025-02-06 14:50:22 -0300

    jodit experiment #1

--- blocks/texto-ricov2/texto-ricov2.js
+++ blocks/texto-ricov2/texto-ricov2.js
@@ -1,5 +1,22 @@
-import { removeDataAueAttributesWhenThereIsFormula } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
-  removeDataAueAttributesWhenThereIsFormula(block);
+  const content = block.children[0];
+  console.log(content)
+
+
+  let editor = document.createElement('textarea');
+  editor.innerHTML = `${content.innerHTML}`
+
+
+  setTimeout(() => {
+      block.appendChild(editor)
+
+      const jodit = Jodit.make(editor, {
+        "toolbarAdaptive": false      });
+      jodit.e.on('blur', param => {
+        content.innerHTML = `${jodit.value.replaceAll("border-collapse:", "border-collapse: ")}`;
+      });
+  }, 1000);
+
+
 }
