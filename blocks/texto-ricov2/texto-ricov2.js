de2deffaba54ed2cb20bba4718635c8aac027a85
Diego Dias <diego.dias@vilt.local>
2025-03-06 11:16:25 -0300

    changed field type to richtext

--- blocks/texto-ricov2/texto-ricov2.js
+++ blocks/texto-ricov2/texto-ricov2.js
@@ -4,24 +4,50 @@ export default function decorate(block) {
   const content = block.children[0];
   console.log(content)
 
+  let editMode = false;
+
 
   let editor = document.createElement('textarea');
-  editor.innerHTML = `${atob(content.textContent)}`
-
+  
+  editor.innerHTML = `${atob(content?.querySelector("pre")?.textContent)}`
+  editor.style.display = "none";
+  
 
   setTimeout(() => {
-      block.appendChild(editor)
+    block.appendChild(editor)
 
-      const jodit = Jodit.make(editor, {
-        "toolbarAdaptive": false      });
-      jodit.e.on('blur', param => {
-        content.querySelector("p").textContent = `${btoa(jodit.value.replaceAll("border-collapse:", "border-collapse: "))}`;
-      });
+    const jodit = Jodit.make(editor, {
+      "toolbarAdaptive": false
+    });
+    jodit.e.on('blur', param => {
+      content.querySelector("pre").textContent = `${btoa(jodit?.value?.replaceAll("border-collapse:", "border-collapse: "))}`;
+    });
+    block.querySelector(".jodit-container").style.display = "none"
+
   }, 1000);
 
-  console.log(content.textContent.trim())
-  const mainContent = document.createElement("div") 
-  mainContent.innerHTML = atob(content.textContent.trim())
+  const mainContent = document.createElement("div")
+  mainContent.innerHTML = atob(content.querySelector("pre").textContent.trim())
   block.append(mainContent)
+
+  const editButton = document.createElement("button")
+  editButton.textContent = "editar com texto rico avançado"
+  block.append(editButton)
+
+  editButton?.addEventListener('click', (e) => {
+    editMode = !editMode
+    if (editMode) {
+      mainContent.style.display = "none"
+      block.querySelector(".jodit-container").style.display = "block"
+      editButton.textContent = "parar de editar"
+
+    } else {
+      mainContent.style.display = "block"
+      block.querySelector(".jodit-container").style.display = "none"
+      editButton.textContent = "editar com texto rico avançado"
+
+    }
+  })
+
 
 }
