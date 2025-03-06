bfcb1cf06953e45aecee2f6206f474588b708938
Diego Dias <diego.dias@vilt.local>
2025-03-06 13:24:46 -0300

    added isInEditor verification to hide stuff on preview and oficial page

--- blocks/texto-ricov2/texto-ricov2.js
+++ blocks/texto-ricov2/texto-ricov2.js
@@ -1,4 +1,4 @@
-import { htmlToElement } from "../../scripts/scripts.js";
+import { enhancedIsInEditor } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
   const content = block.children[0];
@@ -8,10 +8,10 @@ export default function decorate(block) {
 
 
   let editor = document.createElement('textarea');
-  
+
   editor.innerHTML = `${atob(content?.querySelector("pre")?.textContent)}`
   editor.style.display = "none";
-  
+
 
   setTimeout(() => {
     block.appendChild(editor)
@@ -30,24 +30,28 @@ export default function decorate(block) {
   mainContent.innerHTML = atob(content.querySelector("pre").textContent.trim())
   block.append(mainContent)
 
-  const editButton = document.createElement("button")
-  editButton.textContent = "editar com texto rico avançado"
-  block.append(editButton)
+  
+  if (enhancedIsInEditor()) {
+    const editButton = document.createElement("button")
+    editButton.textContent = "editar com texto rico avançado"
+    block.append(editButton)
+    editButton?.addEventListener('click', (e) => {
+      editMode = !editMode
+      if (editMode) {
+        mainContent.style.display = "none"
+        block.querySelector(".jodit-container").style.display = "block"
+        editButton.textContent = "parar de editar"
+  
+      } else {
+        mainContent.style.display = "block"
+        block.querySelector(".jodit-container").style.display = "none"
+        editButton.textContent = "editar com texto rico avançado"
+  
+      }
+    })
+  }
 
-  editButton?.addEventListener('click', (e) => {
-    editMode = !editMode
-    if (editMode) {
-      mainContent.style.display = "none"
-      block.querySelector(".jodit-container").style.display = "block"
-      editButton.textContent = "parar de editar"
-
-    } else {
-      mainContent.style.display = "block"
-      block.querySelector(".jodit-container").style.display = "none"
-      editButton.textContent = "editar com texto rico avançado"
-
-    }
-  })
-
-
+  if (!enhancedIsInEditor()) {
+    content.remove();
+  }
 }
