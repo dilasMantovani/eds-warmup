1f449cfc50f2fe42808f5276aea00a4316c302ea
Diego Dias <diego.dias@vilt.local>
2025-03-14 09:13:33 -0300

    added component for podcast only

--- blocks/podcast/podcast.js
+++ blocks/podcast/podcast.js
@@ -1,12 +1,42 @@
-import { DAM_URL, htmlToElement } from "../../scripts/scripts.js";
+import { htmlToElement, randomString } from "../../scripts/scripts.js";
 
 export default function decorate(block) {
-  const path = block.children[0];
-  const pathText = path.textContent.trim();
-  path.remove();
+    const randomElementID = randomString(10);
 
-  const audioElement = htmlToElement(`<audio controls src="${DAM_URL}${pathText}"></audio>`);
+    const businessKey = block?.children[0]?.textContent?.trim();
+    const mediastreamId = block?.children[1]?.textContent?.trim();
+    const videoLink = block?.children[2]?.textContent?.trim();
+    const captionLink = block?.children[3]?.textContent?.trim();
+    const size = block?.children[4]?.textContent?.trim();
+    const title = block?.children[5]?.textContent?.trim();
+    const description = block?.children[6]?.textContent?.trim();
 
-  block.append(audioElement);
-  
+    block.textContent = "";
+
+
+    if (title) {
+        block.append(htmlToElement(`<p>${title}</p>`))
+    }
+
+    if (mediastreamId) {
+        var script = document.createElement('script');
+        script.src = 'https://player.cdn.mdstrm.com/lightning_player/api.js';
+        script.setAttribute("data-container", randomElementID)
+        script.setAttribute("data-type", "media")
+        script.setAttribute("data-id", mediastreamId)
+        script.setAttribute("data-app-name", "appName")
+        script.setAttribute("id", randomElementID+"-player")
+        script.setAttribute("data-loaded", "playerLoaded")
+    
+        document.head.appendChild(script);
+
+        const playerDiv = document.createElement("div")
+        playerDiv.setAttribute("id", randomElementID);
+        block.append(playerDiv)
+
+    }
+
+    if (description) {
+        block.append(htmlToElement(`<p>${description}</p>`))
+    }
 }
