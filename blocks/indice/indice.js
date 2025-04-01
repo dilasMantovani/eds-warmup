4cf296de644cc71dfca5301b3662e5004e046d97
Diego Dias <diego.dias@vilt.local>
2025-04-01 17:32:49 -0300

    added layout and funcionality to indice component

--- blocks/indice/indice.js
+++ blocks/indice/indice.js
@@ -1,3 +1,76 @@
+import { enhancedIsInEditor, htmlToElement } from "../../scripts/scripts.js";
+
 export default function decorate(block) {
+  const title = block.children[0];
+  const bg = block.children[1];
+  const image = block.children[2];
+
+  title.remove();
+  bg.remove();
+  image.remove();
+
+  const indiceItemList = [];
+  Array.from(block?.children).forEach(element => {
+    indiceItemList.push(element);
+    element.remove();
+  });
+
+
+
+  const capa = document.createElement("div");
+  capa.classList.add("capa");
+
+  if (bg.querySelector("img")) {
+    capa.style.backgroundImage = `url(${bg.querySelector("img").getAttribute('src')})`
+  }
+
+  block.append(capa);
+
+  const box = document.createElement("div");
+  box.classList.add("capa__box");
+  capa.append(box)
+
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("capa__box__image");
+  imageContainer.append(image)
+  box.append(imageContainer)
+  
+  const contentContainer = document.createElement("div");
+  contentContainer.classList.add("capa__box__content");
+  title.classList.add("capa__box__content__title");
+  contentContainer.append(title);
+  
+  const anchorList = document.createElement("div");
+  anchorList.classList.add("capa__box__content__anchor-list");
+  indiceItemList?.forEach(element => {
+    anchorList.append(handleIndiceItem(element));
+  });
+  contentContainer.append(anchorList);
+
+  box.append(contentContainer)
+
+  setTimeout(() => {
+    if (enhancedIsInEditor()) {
+      capa.classList.add("isInEditor");
+    }
+  }, 1500);
+
+}
+
+function handleIndiceItem(indiceItem){
+  const title = indiceItem.children[0];
+  const linkTo = indiceItem.children[1];
+
+  const anchor = document.createElement("a");
+
+  anchor.setAttribute("href", `#${linkTo?.textContent}`);
+  anchor.setAttribute("target", "_self");
+
+  const iconElement = htmlToElement('<i class="fa-solid fa-angle-right"></i>');
+  anchor.append(iconElement)
+  anchor.append(title);
+
+
+  return anchor;
 }
 
