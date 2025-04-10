005cf35b782540f7bd36c89139e69b6237e3933a
Tutu4000 <tutuzinhogamer4000@gmail.com>
2025-04-10 12:41:19 -0300

    test commit

--- blocks/texto-ricov2/texto-ricov2.js
+++ blocks/texto-ricov2/texto-ricov2.js
@@ -2,5 +2,5 @@ import { handleCustomRTE } from "./richTextUtils.js";
 
 export default function decorate(block) {
   if (!block?.children?.[0]) return;
-  handleCustomRTE(block, block.children[0]);
+  handleCustomRTE(null, block.children[0]);
 }
