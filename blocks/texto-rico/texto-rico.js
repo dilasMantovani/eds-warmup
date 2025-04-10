9ca82f38364bdbd286dca40c1c0980e25d7a4824
Tutu4000 <tutuzinhogamer4000@gmail.com>
2025-04-10 13:19:05 -0300

    chagind default text-rico

--- blocks/texto-rico/texto-rico.js
+++ blocks/texto-rico/texto-rico.js
@@ -2,4 +2,18 @@ import { removeDataAueAttributesWhenThereIsFormula } from "../../scripts/scripts
 
 export default function decorate(block) {
   removeDataAueAttributesWhenThereIsFormula(block);
+
+  const firstChild = block.children[0];
+  if (firstChild) {
+    const paragraph = firstChild.querySelector('p');
+    if (paragraph && paragraph.textContent) {
+      const decodedHtml = atob(paragraph.textContent.trim());
+      block.innerHTML = decodedHtml;
+    } else {
+      block.innerHTML = '';
+      firstChild.remove();
+    }
+  } else {
+    block.innerHTML = '';
+  }
 }
