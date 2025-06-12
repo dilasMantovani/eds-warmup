ce1d77da5f2f1f7e7fe2fff183d1cc63d2e06720
Diego Dias <diego.dias@vilt.local>
2025-06-12 14:58:17 -0300

    file reference poc

--- blocks/playground/playground.js
+++ blocks/playground/playground.js
@@ -1,21 +1,3 @@
-import { randomString } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
-  async function getData() {
-    // const url = `http://localhost:666/graphql/execute.json/vilt-group/getTabelaByPath;path=/content/dam/vilt-group/content-fragments/teste-tabela1?1=1`;
-    const url = 'https://publish-p136102-e1378103.adobeaemcloud.com/graphql/execute.json/vilt-group/getTabelaByPath;path=/content/dam/vilt-group/content-fragments/teste-tabela1';
 
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-
-      const json = await response.json();
-      block.innerHTML = json.data.tabelaByPath.item.tabela.html;
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-  getData();
 }
